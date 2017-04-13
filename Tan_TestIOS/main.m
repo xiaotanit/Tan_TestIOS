b5286a6754bd53f2c7b17c65641fa281bfb7b330
M C <MacT@MdeMac-mini.local>
2017-04-13 13:34:46 +0800

    Initial Commit

---
+++ Tan_TestIOS/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Tan_TestIOS
+//
+//  Created by M C on 2017/4/13.
+//  Copyright © 2017年 M C. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
